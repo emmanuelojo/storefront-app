b4ee355f76bc403aa964a68011ba2f36a9bcdef0
emmanuelOjo <emmanuelojo1234@gmail.com>
2024-06-03 12:35:28 +0100

    first commit

---
+++ app.vue
@@ -0,0 +1,19 @@
+<!-- <template>
+  <div>
+    <NuxtWelcome />
+  </div>
+</template> -->
+
+<template>
+    <div class="flex flex-col items-center space-y-4 xs:block xs:space-x-4">
+        <GsButton type="button" variant="primary"> Hello </GsButton>
+        <GsButton type="button" variant="secondary"> Hello </GsButton>
+        <GsButton type="button" variant="tertiary"> Hello </GsButton>
+
+        <product-card-vertical />
+    </div>
+</template>
+
+<script lang="ts" setup>
+// import { SfButton } from "@storefront-ui/vue";
+</script>
